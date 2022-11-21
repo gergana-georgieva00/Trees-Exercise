6a13dabc6b50500c11907c9e4c3a89a6c8303338
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-21 18:28:31 +0200

    Implemented GetPathsWithGivenSum

--- Tree/IntegerTree.cs
+++ Tree/IntegerTree.cs
@@ -12,7 +12,32 @@
 
         public IEnumerable<IEnumerable<int>> GetPathsWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            var result = new List<List<int>>();
+
+            var currPath = new LinkedList<int>();
+            currPath.AddFirst(this.Key);
+            int currSum = this.Key;
+            this.Dfs(this, result, currPath, ref currSum, sum);
+
+            return result;
+        }
+
+        private void Dfs(Tree<int> subtree, List<List<int>> result, LinkedList<int> currPath, ref int currSum, int wantedSum)
+        {
+            foreach (var child in subtree.Children)
+            {
+                currSum += child.Key;
+                currPath.AddLast(child.Key);
+                Dfs(child, result, currPath, ref currSum, wantedSum);
+            }
+
+            if (currSum == wantedSum)
+            {
+                result.Add(new List<int>(currPath));
+            }
+
+            currSum -= subtree.Key;
+            currPath.RemoveLast();
         }
 
         public IEnumerable<Tree<int>> GetSubtreesWithGivenSum(int sum)
